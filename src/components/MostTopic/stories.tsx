1022e37f06a00a63a36228f8283050fbea160430
gabriel.almeida <gabriel.almeida@vexpro.com.br>
2020-10-26 12:55:44 -0300

    MostTopic ajuste story

--- src/components/MostTopic/stories.tsx
+++ src/components/MostTopic/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import UsedTopic, { UsedTopicProps } from '.'
 
 export default {
-  title: 'UsedTopic',
+  title: 'MostTopic',
   component: UsedTopic
 } as Meta
 
