e8d6527f730218bdb633c834312f2695712605af
gabriel.almeida <gabriel.almeida@vexpro.com.br>
2020-10-28 11:07:47 -0300

    Husk ajuste

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,8 +1,6 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Wrapper = styled.img`
-  ${({ theme }) => css`
-    width: 10rem;
-    height: 10rem;
-  `};
+  width: 10rem;
+  height: 10rem;
 `
